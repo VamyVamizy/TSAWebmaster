f98a62c34510f8226966999f4e7ae2cdc81bd900
Carlos Ort-Patrick <27COrtPatrick@live.ytech.edu>
2025-11-21 13:34:48 -0500

    no

---
+++ WebMaster/database/init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    link VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
